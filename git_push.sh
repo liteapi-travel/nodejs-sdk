8c79d8f876831a4752434f219b82130e357c30af
oussa2013 <oussamaqb@Oussamas-MacBook-Pro.local>
2023-07-07 14:53:54 +0100

    updates

--- git_push.sh
+++ git_push.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 # ref: https://help.github.com/articles/adding-an-existing-project-to-github-using-the-command-line/
 #
-# Usage example: /bin/sh ./git_push.sh wing328 openapi-petstore-perl "minor update" "gitlab.com"
 
 git_user_id=$1
 git_repo_id=$2
